e3b7135c071281fd0b5fcd54041096f5ab4b6cd0
Flourence <web3lotttery143@gmail.com>
2025-12-30 17:34:43 +0800

    update: operator address

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,7 +1,7 @@
 //export const VITE_API_URL = "http://192.168.1.27:3000"
 export const VITE_API_URL = "https://web3-lottery-api.blockspacecorp.com"
 export const VITE_WS_PROVIDER = "wss://devnet02.xode.net"
-export const VITE_OPERATOR_ADDRESS = "XqAx4iTUDeWkhVtTGbkBTLpCp7F1BX7ptZpPXNzF4oRs3dsrC"
+export const VITE_OPERATOR_ADDRESS = "XqDGJ69MXL1WhHZiQHsA8HJTu7auK3ZePQZJetMrq3GT5smso"
 export const VITE_WEB_SOCKET_URL = "wss://web3-lottery-api.blockspacecorp.com/lottery-jobs"
 //export const VITE_WEB_SOCKET_URL = "ws://192.168.1.27:3000/lottery-jobs"
 export const VITE_BET_AMOUNT = '0.5'
